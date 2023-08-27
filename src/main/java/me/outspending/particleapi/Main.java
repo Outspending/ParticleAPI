e715eb7e17a8db13a776c49ea4b689cf766c3aa4
Outspending <Dxrippy15@gmail.com>
2023-08-26 21:56:12 -0600

    Some fixes

--- src/main/java/me/outspending/particleapi/Main.java
+++ src/main/java/me/outspending/particleapi/Main.java
@@ -9,5 +9,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        long start = System.currentTimeMillis();
+        ParticleRenderer renderer = CustomParticleType.builder(new CircleParticleType())
+                .editOption(ParticleOption.RADIUS, 15)
+                .editOption(ParticleOption.DENSITY, 360)
+                .build();
+
+        renderer.render(e.getBlock().getLocation());
+        long end = System.currentTimeMillis();
+        Bukkit.broadcastMessage("Rendered in " + (end - start) + "ms");
+    }
 }
